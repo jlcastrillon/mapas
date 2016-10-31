aca412953b2c6e951fa0cca46175ea50d5866861
jlcastrillon <jlcastrillon91@gmail.com>
2016-10-31 21:19:57 +0100

    wer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,12 +4,9 @@ class StaticPagesController < ApplicationController
     #@import = false;
     #if request.post?
     @StaticPage =  StaticPage.import(params[:file])
-    if @StaticPage 
-       flash.now[:notice] = "Import Successful"
-       format.html { render "maps/import", :layout => false  } 
-    else
-       flash[:danger] = "Error Encountered"
-       redirect_to @StaticPage
+    respond_to do |format|
+      format.html
+      format.json
     end
 
   end
