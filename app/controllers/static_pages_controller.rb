bc49a84cb22a37f755c67ac83ba4a370301b9a5c
jlcastrillon <jlcastrillon91@gmail.com>
2016-10-31 23:30:15 +0100

    wer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
-  skip_before_filter :verify_authenticity_token, :only => :create
+  skip_before_filter :verify_authenticity_token
   respond_to :html, :json
   def import
     #@import = false;
