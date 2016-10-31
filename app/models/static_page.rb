1dae0196e24a4e609377ecb581a1ddf396afe793
jlcastrillon <jlcastrillon91@gmail.com>
2016-10-31 23:42:03 +0100

    wer

--- app/models/static_page.rb
+++ app/models/static_page.rb
@@ -4,22 +4,22 @@ class StaticPage
   field :datos, type: Integer
 
   def self.import(file)
-  	CSV.foreach(file.path, headers: true) do |row|
-  		staticpage = find_by_id(row["id"]) || new
-  		 staticpage.attributes = row.to_hash.slice(*accessible_attributes)
-  		 staticpage.save!
+  	#CSV.foreach(file.path, headers: true) do |row|
+  	#	staticpage = find_by_id(row["id"]) || new
+  	#	 staticpage.attributes = row.to_hash.slice(*accessible_attributes)
+  	#	 staticpage.save!
   	end
   end
 
   def self.import(file)
-  	spreadsheet = Roo::Spreadsheet.open(file.path)
-  	header = spreadsheet.row(1)
-  	(2..spreadsheet.last_row).each do |i|
-    	row = Hash[[header, spreadsheet.row(i)].transpose]
-    	staticpage = find_by(id: row["id"]) || new
-    	staticpage.attributes = row.to_hash
-    	staticpage.save!
-  	end
+  	#spreadsheet = Roo::Spreadsheet.open(file.path)
+  	#header = spreadsheet.row(1)
+  	#(2..spreadsheet.last_row).each do |i|
+    #	row = Hash[[header, spreadsheet.row(i)].transpose]
+    #	staticpage = find_by(id: row["id"]) || new
+    #	staticpage.attributes = row.to_hash
+    #	staticpage.save!
+  	#end
   end
 
   def self.open_spreadsheet(file)
