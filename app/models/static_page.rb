d2e78e6a3cb7b07b84e7751ff4b8f702b0e74553
jlcastrillon <jlcastrillon91@gmail.com>
2016-11-01 22:57:53 +0100

    a

--- app/models/static_page.rb
+++ app/models/static_page.rb
@@ -1,26 +1,26 @@
 class StaticPage
   include Mongoid::Document
   field :nombre, type: String
-  field :datos, type: Binary
-
-  def self.import(file)
-  	CSV.foreach(file.path, headers: true) do |row|
-  		staticpage = find_by_id(row["id"]) || new
-  		 staticpage.attributes = row.to_hash.slice(*accessible_attributes)
-  		 staticpage.save!
-  	end
-  end
+  field :datos, type: Integer
 
   #def self.import(file)
-  #	spreadsheet = Roo::Spreadsheet.open(file.path)
-  #	header = spreadsheet.row(1)
-  #	(2..spreadsheet.last_row).each do |i|
-   # 	row = Hash[[header, spreadsheet.row(i)].transpose]
-    #	staticpage = find_by(id: row["id"]) || new
-    #	staticpage.attributes = row.to_hash
-    #	staticpage.save!
+  #	CSV.foreach(file.path, headers: true) do |row|
+  #		staticpage = find_by_id(row["id"]) || new
+  #		 staticpage.attributes = row.to_hash.slice(*accessible_attributes)
+  #		 staticpage.save!
   #	end
   #end
+
+  def self.import(file)
+  	spreadsheet = Roo::Spreadsheet.open(file.path)
+  	header = spreadsheet.row(1)
+  	(2..spreadsheet.last_row).each do |i|
+    	row = Hash[[header, spreadsheet.row(i)].transpose]
+    	staticpage = find_by(id: row["id"]) || new
+    	staticpage.attributes = row.to_hash
+    	staticpage.save!
+  	end
+  end
 
   def self.open_spreadsheet(file)
   	case File.extname(file.original_filename)
