e3dc17b1b5d57a90f22831214cde3a407c7317b7
jlcastrillon <jlcastrillon91@gmail.com>
2016-11-01 23:29:04 +0100

    a

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,3 +1,11 @@
 class Document
   include Mongoid::Document
+  def self.import(upload)
+    name =  upload['datafile'].original_filename
+    directory = "public/data"
+    
+    path = File.join(directory, name)
+    
+    File.open(path, "wb") { |f| f.write(upload['datafile'].read) }
+  end
 end
