6e03a6abffbd827f4167831ade86ef720e44b71e
jlcastrillon <jlcastrillon91@gmail.com>
2016-11-01 22:01:34 +0100

    a

---
+++ app/models/document.rb
@@ -0,0 +1,10 @@
+class Document < ActiveRecord::Base
+  def self.import(upload)
+    name =  upload['datafile'].original_filename
+    directory = "public/data"
+    # create the file path
+    path = File.join(directory, name)
+    # write the file
+    File.open(path, "wb") { |f| f.write(upload['datafile'].read) }
+  ends
+end
