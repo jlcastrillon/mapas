c92b9150cde5786ba9fdc20691535aebb23e8e76
jlcastrillon <jlcastrillon91@gmail.com>
2016-11-01 19:08:02 +0100

    asd

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,12 @@
 class StaticPagesController < ApplicationController
   skip_before_filter :verify_authenticity_token
-  respond_to :html,:json
   def import
     #@import = false;
     #if request.post?
     @import =  StaticPage.import(params[:file])
     respond_to do |format|
-      #format.html{ render :nothing => true, :status => 201 }
       format.js { redirect_to  '/static_pages/import' }
+      format.html
     end
 
   end
