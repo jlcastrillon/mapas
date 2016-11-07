8e4357354cb871f475ee074687f17e190fa2d4dc
jlcastrillon <jlcastrillon91@gmail.com>
2016-11-07 01:51:31 +0100

    q

--- spec/factories/documents.rb
+++ spec/factories/documents.rb
@@ -1,12 +1,8 @@
 FactoryGirl.define do
   factory :document do
-<<<<<<< HEAD
     filename "MyString"
     content_type "MyString"
     file_contents ""
-=======
-    
->>>>>>> 93531c41c05c01ee21d60fe7a10dd3c28eb71abb
   end
 end
 
